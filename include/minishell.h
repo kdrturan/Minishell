43d97f82bf9d45e3ef446f42247299132b39535e
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-05-27 04:53:20 +0300

    norm

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 22:29:07 by tuaydin           #+#    #+#             */
-/*   Updated: 2025/05/26 22:29:08 by tuaydin          ###   ########.fr       */
+/*   Updated: 2025/05/27 01:59:19 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define MINISHELL_H
 
 # include "libft.h"
+# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
-# include <stdio.h>
 # include <stdlib.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,26 @@
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 22:29:43 by tuaydin           #+#    #+#             */
-/*   Updated: 2025/05/26 22:29:44 by tuaydin          ###   ########.fr       */
+/*   Updated: 2025/05/27 02:53:47 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "token.h"
+#include "signal_handler.h"
 
 int	main(int ac, char **av, char **env)
 {
+	set_signals(INTERACTIVE);
+
+	char *temp;
+	while (1)
+	{
+		temp = readline("MINISHELL>");
+		if (temp == NULL)
+			exit (0);
+		printf("%s\n",temp);
+		free(temp);
+	}
 	return (0);
 }
